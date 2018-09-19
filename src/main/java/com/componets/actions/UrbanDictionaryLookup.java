5f49e028de6c898c00184841eef66ffca6964e46
geraldjglasgow@gmail.com <gy3ve8ls59>
2018-09-18 23:49:25 -0400

    - adding classes for retrieving definitions

--- src/main/java/com/componets/actions/UrbanDictionaryLookup.java
+++ src/main/java/com/componets/actions/UrbanDictionaryLookup.java
@@ -1,6 +1,12 @@
 package com.componets.actions;
 
 public class UrbanDictionaryLookup {
+
+    private String word;
+
+    public UrbanDictionaryLookup(String word){
+        this.word = word;
+    }
     //https://github.com/zdict/zdict/wiki/Urban-dictionary-API-documentation
     //https://github.com/stleary/JSON-java
 
