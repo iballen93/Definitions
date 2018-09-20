3b742865b157b0c84ead851e2f1b2febb6b1a77b
glasgoge <geraldjglasgow@gmail.com>
2018-09-20 14:26:45 -0400

    - adding in urban dictionary lookup

--- src/main/java/com/componets/actions/HelloAction.java
+++ src/main/java/com/componets/actions/HelloAction.java
@@ -1,5 +1,9 @@
 package com.componets.actions;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
@@ -11,13 +15,17 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 
 import javax.swing.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class HelloAction extends AnAction {
 
     private String elementKeyword;
     private String elementText;
-    private String thssf;
 
     public void actionPerformed(AnActionEvent e) {
         getPsiClassFromContext(e);
@@ -26,7 +34,7 @@ public class HelloAction extends AnAction {
 
     private void displayDialog() {
         JFrame frame = new JFrame("FrameDemo");
-        JOptionPane.showMessageDialog(frame, "Term: " + elementText + "\nDefinition: " + getJavaDefinition(elementText));
+        JOptionPane.showMessageDialog(frame, "Term: " + elementText + "\nDefinition: " + getJavaDefinition(elementText) + "\n\n" + "Urban Term: " + elementText + "\nDefinition " + getDefinitionFromJsonString(getJSONString("http://api.urbandictionary.com/v0/define?term={" + elementText + "}")).replace("\n\n", "\n"));
     }
 
     @Override
@@ -46,6 +54,7 @@ public class HelloAction extends AnAction {
 
         setElementText(psiElement.getText().replace("\"", ""));
 
+        //TODO
         /*try {
             setElementKeyword(((PsiKeywordImpl) psiElement).getTokenType().toString());
         } catch (Exception e) {
@@ -70,5 +79,38 @@ public class HelloAction extends AnAction {
 
     private void setElementKeyword(String elementKeyword) {
         this.elementKeyword = elementKeyword;
+    }
+
+    private String getDefinitionFromJsonString(String JsonString) {
+        JsonObject jsonObject = new Gson().fromJson(JsonString, JsonObject.class);
+        JsonArray jsonArray = jsonObject.getAsJsonArray("list");
+        jsonObject = jsonArray.get(0).getAsJsonObject();
+        System.out.println(jsonObject.get("definition").getAsString());
+
+        return jsonObject.get("definition").getAsString();
+    }
+
+    private String getJSONString(String url){
+        String JsonString = "";
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestMethod("GET");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+            StringBuffer sb = new StringBuffer();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                JsonString += line;
+            }
+        } catch (MalformedURLException e){
+
+        } catch (IOException e){
+
+        }
+        return JsonString;
     }
 }
