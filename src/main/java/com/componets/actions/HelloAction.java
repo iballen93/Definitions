3faa2860727bc95ee2089175fc7f7dc0bdf68c41
allenbr <iballen93@gmail.com>
2018-09-21 10:15:10 -0400

    removing import

--- src/main/java/com/componets/actions/HelloAction.java
+++ src/main/java/com/componets/actions/HelloAction.java
@@ -2,7 +2,6 @@ package com.componets.actions;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
