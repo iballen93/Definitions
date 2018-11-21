161a12f65fb4a682054df599126ac7ee02c33618
allenbr <iballen93@gmail.com>
2018-11-21 12:13:04 -0500

    adding action to Tools Menu. skeleton for toggling definition sources

---
+++ src/main/java/com/components/actions/ToolsMenuAction.java
@@ -0,0 +1,14 @@
+package com.components.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class ToolsMenuAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+        // look at View menu in PlatformActions.xml and how it toggles: Toolbar, Tool Buttons, Status Bar, Navigation Bar
+        // extend ToggleAction
+    }
+}
